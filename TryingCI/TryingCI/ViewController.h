9b3642a65a9449eb45ff3835f80db27b3f95d18f
Vinh Huynh <vinhhuynh115@gmail.com>
2017-12-01 10:20:47 +0700

    Added xcode project

---
+++ TryingCI/TryingCI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryingCI
+//
+//  Created by Vinh Huynh on 12/1/17.
+//  Copyright © 2017 Vinh Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TryingCI/TryingCI/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TryingCI
+//
+//  Created by Vinh Huynh on 12/1/17.
+//  Copyright © 2017 Vinh Huynh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
